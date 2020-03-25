a0608e68d0f752ade84559797c0af1060c331714
Andrei Korolevich <korolevich1994@mail.ru>
2020-03-25 10:54:56 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,41 @@
+'use strict';
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let rootFirst;
+    let rootSecond;
+    let discriminant = Math.pow(b,2) - (4 * a * c);
+    console.log(discriminant);
+    if(discriminant === 0) {
+        rootFirst = -b / (2 * a);
+        x.push(rootFirst);
+    }else if (discriminant > 0) {
+        rootFirst = (-b + Math.sqrt(discriminant)) / (2 * a);  
+        rootSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+        x.push(rootFirst, rootSecond);
+    }
+     return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = 0;
+    if (marks.length > 5) {
+        console.log(`Отметок больше 5 - ${marks}`);
+        marks = marks.slice(0,5);
+        averageMark = marks.reduce((sum, element) => sum + element, 0) / 5;
+    }else if (marks.length > 0) {
+        averageMark = marks.reduce((sum, element) => sum + element, 0) / marks.length ;
+    }
+     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let result;
+    let date = new Date();
+    let age = date.getFullYear() - dateOfBirthday.getFullYear();
+    if(age >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    }else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+     return result;
 }
