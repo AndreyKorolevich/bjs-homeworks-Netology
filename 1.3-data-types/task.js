0771f0927d4c349a0c36766016aa0b9329507a86
Andrei Korolevich <korolevich1994@mail.ru>
2020-03-27 12:48:25 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let percents = Number(percent) / (12 * 100);
+    let contributions = Number(contribution);
+    let amounts = Number(amount);
+    let nowDate = new Date();
+    let totalAmount;
+    if (isNaN(percents)) {
+        totalAmount = `Параметр percent 
+        содержит неправильное значение ${percents}`;
+    } else if (isNaN(contributions)) {
+        totalAmount = `Параметр contribution 
+        содержит неправильное значение ${contributions}`;
+    } else if (isNaN(amounts)) {
+        totalAmount = `Параметр amount 
+        содержит неправильное значение ${amounts}`;
+    }
+
+    let amountMonth = Math.floor((date.getTime() - nowDate) / (1000 * 60 * 60 * 24 * 30));
+    let bodyCredit = amounts -  contributions;
+    let monthlyPayment = bodyCredit * (percents + percents / ((Math.pow((1 + percents), amountMonth)) - 1));
+    console.log(monthlyPayment);
+    totalAmount = (amountMonth * monthlyPayment);
+    console.log(totalAmount);
+    totalAmount = Number(totalAmount.toFixed(2));
+
+     return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if(name == '' || name == undefined || name == null) {
+        name = 'Аноним';
+    }
+    let greeting = `Привет, мир! Меня зовут ${name}`
+     return greeting;
 }
