86006a24748a0b5db104f1a217a89801a5efe6bb
Andrei Korolevich <korolevich1994@mail.ru>
2020-04-02 21:31:49 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,28 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+    let sound = animal.sound;
+    return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+    let average = marks.reduce(function(sum, element){
+        return sum += Number(element);
+    },0) / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date().getTime();
+    let birthdays = new Date(birthday).getTime();
+    let diff = (now - birthdays);
+    let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+    if(age > 18) {
+        return age;
+    }
 }
