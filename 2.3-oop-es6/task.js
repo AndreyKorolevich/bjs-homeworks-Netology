cf55ec965ca59fecf248f5989462134c241cfc1c
Andrei Korolevich <korolevich1994@mail.ru>
2020-04-07 14:54:49 +0300

    Fixed homework 2.3-oop-es6

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,11 +1,11 @@
 // Task N1
 class Weapon {
-    constructor(name, attack, durability, range) {
-        this.name = name;
-        this.attack = attack;
-        this.durability = durability;
-        this.startDurability = durability;
-        this.range = range;
+    constructor(weapon) {
+        this.name = weapon.name;
+        this.attack = weapon.attack;
+        this.durability = weapon.durability;
+        this.startDurability = weapon.durability;
+        this.range = weapon.range;
     }
 
     takeDamage(damage) {
@@ -16,13 +16,13 @@ class Weapon {
     }
 
     getDamage() {
-        if ((this.durability / this.startDurability) >= 0.3) {
-            return this.attack;
-        } else if (this.durability === 0) {
+        if(this.durability === 0){
             return 0;
-        } else {
-            return (this.attack = this.attack / 2);
         }
+        if (isNaN(this.durability / this.startDurability) || ((this.durability / this.startDurability) >= 0.3)) {
+            return this.attack;
+        }
+            return (this.attack = this.attack / 2);
     }
 
     isBroken() {
@@ -33,15 +33,56 @@ class Weapon {
     }
 }
 
-const arm = new Weapon('Рука', 1, Infinity, 1);
-const bow = new Weapon('Лук', 10, 200, 3);
-const sword = new Weapon('Меч', 25, 500, 1);
-const knife = new Weapon('Нож', 5, 300, 1);
-const staff = new Weapon('Посох', 8, 300, 2);
+const arm = new Weapon({
+    name: 'Рука',
+    attack: 1,
+    durability: Infinity,
+    range: 1,
+});
+const bow = new Weapon({
+    name: 'Лук',
+    attack: 10,
+    durability: 200,
+    range: 3,
+});
+const sword = new Weapon({
+    name: 'Меч',
+    attack: 25,
+    durability: 500,
+    range: 1,
+});
+const knife = new Weapon({
+    name: 'Нож',
+    attack: 5,
+    durability: 300,
+    range: 1,
+});
+const staff = new Weapon({
+    name: 'Посох',
+    attack: 8,
+    durability: 300,
+    range: 2,
+});
 
-const uberBow = new Weapon('Длинный лук', 15, bow.durability, 4);
-const uberSword = new Weapon('Секира', 27, 800, sword.range);
-const uberStaff = new Weapon('Посох Бури', 10, staff.durability, 3);
+const uberBow = new Weapon({
+    name: 'Длинный лук',
+    attack: 15,
+    durability: bow.durability,
+    range: 4,
+});
+const uberSword = new Weapon({
+    name: 'Секира',
+    attack: 27,
+    durability: 800,
+    range: sword.range,
+});
+const uberStaff = new Weapon({
+    name: 'Посох Бури',
+    attack: 10,
+    durability: staff.durability,
+    range: 3,
+});
+
 console.log(uberBow);
 console.log(uberSword);
 console.log(uberStaff);
@@ -76,23 +117,107 @@ console.log(arm.durability);
 console.log(arm.isBroken());
 
 // Task 2
-class SimpleWeapon extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range);
+class Arm extends Weapon {
+    constructor() {
+        super({
+            name: 'Рука',
+            attack: 1,
+            durability: Infinity,
+            range: 1,
+        });
     }
 }
 
-class UberWeapon extends SimpleWeapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range);
+class Bow extends Weapon {
+    constructor() {
+        super({
+            name: 'Лук',
+            attack: 10,
+            durability: 200,
+            range: 3,
+        });
     }
 }
 
-const uberKnife = new UberWeapon('Супер нож', 20, 2, 5);
-console.log(uberKnife.name); // Лук
-console.log(uberKnife.attack); // 10
-console.log(uberKnife.durability); // 200
-console.log(uberKnife.range);
+class Sword extends Weapon {
+    constructor() {
+        super({
+            name: 'Меч',
+            attack: 25,
+            durability: 500,
+            range: 1,
+        });
+    }
+}
+
+class Knife extends Weapon {
+    constructor() {
+        super({
+            name: 'Нож',
+            attack: 5,
+            durability: 300,
+            range: 1,
+        });
+    }
+}
+
+class Staff extends Weapon {
+    constructor() {
+        super({
+            name: 'Посох',
+            attack: 8,
+            durability: 300,
+            range: 2,
+        });
+    }
+}
+
+class LongBow extends Bow {
+    constructor() {
+        super();
+        this.name = 'Длинный лук';
+        this.attack = 15;
+        this.range = 4;
+
+    }
+}
+
+class Axe extends Sword {
+    constructor() {
+        super();
+        this.name = 'Секира';
+        this.attack = 27;
+        this.durability = 800;
+    }
+}
+
+class StormStaff extends Staff {
+    constructor() {
+        super();
+        this.name = 'Посох Бури';
+        this.attack = 10;
+        this.range = 3;
+
+    }
+}
+
+
+
+const arm1 = new Arm();
+const bow1 = new Bow();
+const sword1 = new Sword();
+const knife1 = new Knife();
+const staff1 = new Staff();
+
+console.log(arm1);
+
+const uberBow1 = new LongBow();
+const uberSword1 = new Axe();
+const uberStaff1 = new StormStaff();
+console.log(uberBow1.name);
+console.log(uberBow1.attack);
+console.log(uberBow1.durability);
+console.log(uberBow1.range);
 
 // Task N3
 class StudentLog {
@@ -108,8 +233,8 @@ class StudentLog {
     addGrade(grade, subject) {
         if (isNaN(grade) || (grade > 5) || (grade < 1)) {
             console.log(` Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.`);
-            if(this.marks[subject].length != 0) {
-            return this.marks[subject].length;
+            if (this.marks[subject].length != 0) {
+                return this.marks[subject].length;
             }
             return 0;
         }
@@ -126,7 +251,7 @@ class StudentLog {
     getAverageBySubject(subject) {
         for (const sub in this.marks) {
             if (sub === subject) {
-                return (this.marks[sub].reduce((sum,elem) => sum + elem,0) / this.marks[sub].length);
+                return (this.marks[sub].reduce((sum, elem) => sum + elem, 0) / this.marks[sub].length);
             }
         }
         return 0;
@@ -137,13 +262,13 @@ class StudentLog {
         let count = 0;
         for (const s of Object.keys(this.marks)) {
             console.log(s);
-            if(s == 'sub') {
+            if (s == 'sub') {
                 continue;
             }
             sumAll += this.getAverageBySubject(s);
             count++;
         }
-        if( sumAll === 0) {
+        if (sumAll === 0) {
             return 0;
         }
         return (sumAll / count)
