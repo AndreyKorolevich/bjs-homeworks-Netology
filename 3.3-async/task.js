4c85e6c706447c365325a3ade47ea216ef05b254
Andrei Korolevich <korolevich1994@mail.ru>
2020-04-12 12:28:28 +0300

    Fixed metodh addClock homework 3.3-async

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -5,20 +5,25 @@ class AlarmClock {
     }
 
     addClock(startAct, act, iD) {
-        if (iD === undefined) {
-            throw new Error('ID isn`t defined');
+        try {
+            if (iD === undefined) {
+                throw new Error('ID isn`t defined');
+            }
+            if (this.alarmCollection.find(elem => elem.id === iD) !== undefined) {
+                throw new Error('This ID has already been defined');
+            }
+            this.alarmCollection.push({
+                id: iD,
+                time: startAct,
+                callback: act,
+            })
+            console.log(this.alarmCollection);
+        } catch (err) {
+            console.error(err);
         }
-        if (this.alarmCollection.find(elem => elem.id === iD) !== undefined) {
-            throw new Error('This ID has already been defined');
-        }
-
-        this.alarmCollection.push({
-            id: iD,
-            time: startAct,
-            callback: act,
-        })
-        console.log(this.alarmCollection);
         return;
+
+
     }
 
     removeClock(iD) {
@@ -46,7 +51,7 @@ class AlarmClock {
             }
         }
         if (this.timerId === null) {
-            this.timerId = setInterval(() => this.alarmCollection.forEach(elem => checkClock(elem)),1000);
+            this.timerId = setInterval(() => this.alarmCollection.forEach(elem => checkClock(elem)), 1000);
             console.log(this.timerId);
         }
 
@@ -74,14 +79,12 @@ phoneAlarm.addClock('10:12', () => console.log('We`re going to bed soon'), 1);
 phoneAlarm.addClock('10:13', () => {
     console.log('It`s time to go to bed'), phoneAlarm.removeClock(2)
 }, 2);
-//phoneAlarm.addClock('16:01', () => console.log('Go to wash'));
+phoneAlarm.addClock('16:01', () => console.log('Go to wash'));
 phoneAlarm.addClock('10:14', () => {
     console.log('Go to bed, you`re going to get up early tomorrow');
     phoneAlarm.clearAlarms();
     phoneAlarm.printAlarms();
 }, 3);
-//phoneAlarm.addClock('16:05', () => console.log('Go to bed, you`re going to get up early tomorrow'),3);
+phoneAlarm.addClock('16:05', () => console.log('Go to bed, you`re going to get up early tomorrow'), 3);
 phoneAlarm.printAlarms();
 phoneAlarm.start();
-
-
